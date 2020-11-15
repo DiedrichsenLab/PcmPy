Fixed trace(A@B) in second derivative

inference.py
@@ -128,7 +128,7 @@ def likelihood_individ(theta, M, YY, Z, X=None,
     # Based on iVdV we can get he first derivative
     dLdtheta = np.zeros((n_param,))
     for i in range(n_param):
-        dLdtheta[i] = -n_channel / 2 * trace(iVdV[i]) + 0.5 * einsum('ij,ij->',iVdV[i], B)
+        dLdtheta[i] = -n_channel / 2 * trace(iVdV[i]) + 0.5 * einsum('ij,ij->',iVdV[i], B) # Trace(A@B.T)
     if fit_scale:
         dLdtheta[indx_scale] -= scale_param / scale_prior
 
@@ -140,7 +140,7 @@ def likelihood_individ(theta, M, YY, Z, X=None,
     d2L = np.zeros((n_param,n_param))
     for i in range(n_param):
         for j in range(i, n_param):
-            d2L[i, j] = -n_channel / 2 * einsum('ij,ij->',iVdV[i],iVdV[j])
+            d2L[i, j] = -n_channel / 2 * einsum('ij,ji->',iVdV[i],iVdV[j]) # Trace(A@B)
             d2L[j, i] = d2L[i, j]
     if fit_scale:
         d2L[indx_scale, indx_scale] -= 1 / scale_prior

regression.py
@@ -112,7 +112,7 @@ def likelihood_diagYYT_ZZT(theta, Z, YY, num_var, comp, X=None, Noise=pcm.model.
     # Based on iVdV we can get he first derivative
     dLdtheta = np.zeros((n_param,))
     for i in range(n_param):
-        dLdtheta[i] = -num_var / 2 * trace(iVdV[i]) + 0.5 * einsum('ij,ij->',iVdV[i], B)
+        dLdtheta[i] = -num_var / 2 * trace(iVdV[i]) + 0.5 * einsum('ij,ij->',iVdV[i], B) # Trace(A@B.T)
 
     # If only first derivative, exit here
     if return_deriv == 1:
@@ -122,7 +122,7 @@ def likelihood_diagYYT_ZZT(theta, Z, YY, num_var, comp, X=None, Noise=pcm.model.
     d2L = np.zeros((n_param,n_param))
     for i in range(n_param):
         for j in range(i, n_param):
-            d2L[i, j] = -num_var / 2 * einsum('ij,ji->',iVdV[i],iVdV[j])
+            d2L[i, j] = -num_var / 2 * einsum('ij,ji->',iVdV[i],iVdV[j]) # Trace(A@B)
             d2L[j, i] = d2L[i, j]
 
     if return_deriv == 2:
@@ -440,15 +440,16 @@ class RidgeDiag:
             else:
                 like_fcn = 'YYT'
 
-        if like_fcn == 'YTY':
-            fcn = lambda x: likelihood_diagYTY(x, Z, Y, self.components, X, self.noise_model, return_deriv=2)
-        elif like_fcn == 'YYT':
+        if like_fcn == 'YYT_ZZT':
             YY = Y @ Y.T
-            fcn = lambda x: likelihood_diagYYT(x, Z, YY, P, self.components, X, self.noise_model, return_deriv=2)
-        elif like_fcn == 'YTY1':
-            fcn = lambda x: likelihood_diagYTY1(x, Z, Y, self.components, X, self.noise_model, return_deriv=2)
-        elif like_fcn == 'YTY2':
-            fcn = lambda x: likelihood_diagYTY2(x, Z, Y, self.components, X, self.noise_model, return_deriv=2)
+            fcn = lambda x: likelihood_diagYYT_ZZT(x, Z, YY, P, self.components, X, self.noise_model, return_deriv=2)
+        elif like_fcn == 'YYT_ZTZ':
+            YY = Y @ Y.T
+            fcn = lambda x: likelihood_diagYYT_ZTZ(x, Z, YY, P, self.components, X, self.noise_model, return_deriv=2)
+        elif like_fcn == 'YTY_ZZT':
+            fcn = lambda x: likelihood_diagYTY_ZZT(x, Z, Y, self.components, X, self.noise_model, return_deriv=2)
+        elif like_fcn == 'YTY_ZTZ':
+            fcn = lambda x: likelihood_diagYTY_ZTZ(x, Z, Y, self.components, X, self.noise_model, return_deriv=2)
         else:
             raise NameError('like_fcn needs to be auto, YYT, or YTY')
         self.theta_, self.trainLogLike_, self.optim_info = pcm.optimize.newton(self.theta0_, fcn, **optim_param)

tests/test_regression_performance.py
@@ -135,7 +135,7 @@ def pcm_performance(comp, theta, num_sim = 10, N = 50, P = 100, fixed_effect = N
     return T
 
 def likelihood_compare(comp, theta, num_sim = 10, N = 50, P = 100,
-                       fixed_effect = None, fit_intercept = False, likefcn=['YYT','YTY']):
+                       fixed_effect = None, fit_intercept = False, likefcn=['YYT_ZZT','YYT_ZTZ','YTY_ZZT','YTY_ZTZ']):
     Q = comp.shape[0]
     s = sqrt(exp(theta[comp]))
     z = np.zeros((num_sim*2,))
@@ -165,6 +165,7 @@ def likelihood_compare(comp, theta, num_sim = 10, N = 50, P = 100,
             M.optimize_regularization(Z,Y,like_fcn = lf)
             T.loc[idx,'time_Pcm'] = time.perf_counter() - t0
             for k in range(n_param):
-                T.loc[idx,'theta'+str(k)]=M.theta_[k]
-            T.loc[idx,'likefcn']=lf
+                T.loc[idx,'theta'+str(k)] = M.theta_[k]
+            T.loc[idx,'likefcn'] = lf
+            T.loc[idx,'iter'] = M.optim_info['iter']
     return T